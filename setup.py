cdfa195c2358f18291075ab670a03526a7a2c108
minishrink <mathur_akanksha@hotmail.com>
2019-01-18 21:50:51 +0000

    setup file
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ETAS',
+    version='1',
+    packages=['pytest'],
+    url='',
+    license='',
+    author='Akanksha Mathur',
+    author_email='mathur_akanksha@hotmail.com',
+    description='Commandline tool to generate static HTML calendars for given years under Gregorian calendar'
+)
